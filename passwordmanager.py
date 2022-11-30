19ec9a0dc1f6639d06153f4599ae919ed31621b3
tonibraeutigam <toni.braeutigam@googlemail.com>
2022-11-30 10:46:22 +0100

    update method show_existing_pwd

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,17 +1,16 @@
 from os.path import exists
 import glob
 
-
 def main():
     exit = False 
     while exit == False:
         choice = mainmenu()
         if choice == "1":
             filename = create_database()
-            #print(filename)
             mainmenu_two(filename)
         elif choice == "2":
-            open_database()
+            exist_file_name = open_database()
+            mainmenu_two(exist_file_name)
         elif choice == "3":
             exit = True
         else:
@@ -24,7 +23,7 @@ def mainmenu():
     print("1) Create a new password database")
     print("2) Start with an existing database")
     print("3) cancel")
-    
+    2
     return str(input("What do you want to do ?: "))
 
 def create_database():
@@ -39,19 +38,16 @@ def open_database():
     for file in glob.glob("*.txt"):
         txtfiles.append(file)
     print(txtfiles)
-    open_file = str(input("tip down the name of the databank you want to use: "))
-
-    f = open(open_file, "r") #read
-    for line in f: 
-        print(line.rstrip()) # rstrip entfernt Leerzeichen und Zeilenumbruche vom rechten Ende der Zeile
-    f.close()
-
-def mainmenu_two(file_name):
+    exist_file_name = str(input("tip down the name of the databank you want to use: "))
+    #if not exist ? 
+    return exist_file_name
     
+
+def mainmenu_two(exist_file_name): 
     exit = False 
     while exit == False:    
         print("===========================================")
-        print("             Passwordmaganager " + str(file_name))
+        print("             Passwordmaganager " + str(exist_file_name))
         print("===========================================")
         print(" 1) Show existing passwords")
         print(" 2) Add new password ")
@@ -62,12 +58,10 @@ def mainmenu_two(file_name):
         choice = str(input("What do you like to do?: "))
 
         if choice == "1":
-            filename = show_existing_password(file_name)
-            #print(filename)
-            #doc_header()
-            mainmenu_two(file_name)
+            show_existing_password(exist_file_name)
+            mainmenu_two(exist_file_name)
         elif choice == "2":
-            add_new_password(file_name)
+            add_new_password(exist_file_name)
         elif choice == "3":
             delete_existing_password()
         elif choice == "4":
@@ -77,28 +71,31 @@ def mainmenu_two(file_name):
         else:
             print ("Invalid Input")
 
-def show_existing_password(file_name):
-    f = open(file_name, "r") #read
-    for line in f: 
-        print(line.rstrip()) # rstrip entfernt Leerzeichen und Zeilenumbruche vom rechten Ende der Zeile
-        f.close()
-        return file_name
-    #Daten müssen formatiert angezeigt werden 
+def show_existing_password(exist_file_name):
+    doc_header()
+    f = open(exist_file_name,'r')
+    count = 0 
+    for line in f:
+        count += 1 
+        data = line.strip().split(';')
+        data_format = "{0:10s}{1:10s}{2:25s}{3:30s}{4:50s}".format(str(count),data[0],data[1],data[2],data[3])
+        print(data_format)
+    f.close()
+
+        #data_format = "{0:10s}{1:10s}{2:25s}{3:320s}{4:50s}".format("index",data[0],data[1],data[2],data[3])
+        #print(data_format)
+        #return exist_file_name
+   
 
 def add_new_password(file_name):
-    f = open(file_name, "a") #append
+    f = open(file_name, "a") #append1
     name = (str(input("Please enter your name:")))
     password = (str(input("Please enter your password:")))
     url = (str(input("Please enter your URL:")))
     note = (str(input("Please enter your Note:")))
-    pw_data = f.write(str(name + ":" + password + ":" + url + ":" + note))
+    pw_data = f.write(str(name + ";" + password + ";" + url + ";" + note + "\n"))
     f.close()
-    #4x Input + Text Name?, Password?, Url?, Note?
-    #zusammenfassen in einen Variable(String) 
-    #mit append f = open(file_name, "a") #append
-    #f.write(str() + "\n") //hier wird der String(Variable) hinzugefügt
-    #f.close()
-    return pw_data
+    return file_name
 
 def delete_existing_password():
     pass
@@ -109,11 +106,13 @@ def update_password():
 def doc_header():
     print("--------------------------------------------------------------------------------------")
     header = "{0:10s}{1:10s}{2:25s}{3:30s}{4:50s}".format("Index","Name","Password","URL","Note")
+    print(header)
     print("--------------------------------------------------------------------------------------")
     return header
 
 
 
-#print (doc_header())
+#(doc_header())
 main()
 #open_database()
+#show_existing_password()
